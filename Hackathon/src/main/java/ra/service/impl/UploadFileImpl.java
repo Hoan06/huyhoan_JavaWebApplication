48d41b56ec2ec2c4da11385b898dc0a28e42fb3c
unknown <huyhoan13032006@gmail.com>
2026-04-19 22:01:10 +0700

    up

---
+++ Hackathon/src/main/java/ra/service/impl/UploadFileImpl.java
@@ -0,0 +1,40 @@
+package ra.service.impl;
+
+import jakarta.servlet.ServletContext;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ra.service.UploadFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class UploadFileImpl implements UploadFile {
+    ServletContext servletContext;
+
+    public UploadFileImpl(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
+
+    String destPath = "";
+
+    @Override
+    public String uploadToLocal(MultipartFile file) {
+        if (file != null && !file.isEmpty()) {
+            String realPath = servletContext.getRealPath("/resources/images/");
+
+            File f = new File(realPath);
+            if (!f.exists()) {
+                f.mkdirs();
+            }
+            destPath = f.getAbsolutePath() +  "/" + file.getOriginalFilename();
+            try {
+                file.transferTo(new File(destPath));
+                return file.getOriginalFilename();
+            } catch (IOException e) {
+                System.out.println("Load Fail !" + e.getMessage());
+            }
+        }
+        return "";
+    }
+}
